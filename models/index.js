45cc92e8767afcdd8f99f99fc7c50e4106220b27
newms34 <newms3450@gmail.com>
2016-05-24 19:34:39 -0400

    removed auto-seed, since it's easy enough to just do this manually (and probly safer)

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV && process.env.NODE_ENV == 'development') {
     //just some quick env check. If we're developing locally, go ahead and use our local db. Otherwise, use the mlab db.
     mongoose.connect('mongodb://localhost:27017/mazeGame');
 } else {
-    mongoose.connect('err');
+    mongoose.connect('mongodb://mazeMaker:maze34@ds011913.mlab.com:11913/heroku_701xzs88');
 }
 var db = mongoose.connection;
 
